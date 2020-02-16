02b66abc2805140d05ddf7d7513ff8acd728363a
pooyagheyami <pooyagheyami@gmail.com>
2020-02-16 12:06:21 +0200

    Add txt parse

--- VicFskWeb/app.py
+++ VicFskWeb/app.py
@@ -1,7 +1,9 @@
 # In the name of God
 
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request , get_flashed_messages
+from jinja2 import TemplateNotFound
 import os
+import parsetxt
 
 Engmenu = []
 Tmlmenu = []
@@ -11,7 +13,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     title = 'Victorian Tamil'
-    return render_template("index.html", mytitle=title)
+    author , title , contxt = testi()
+
+    try:
+        return render_template("index.html", mytitle=title , txtsi=[author,title,contxt])
+    except TemplateNotFound:
+        return 'I Do Not Find This page' #abort(404)
 
 @app.route("/#Tml")
 def index2():
@@ -19,9 +26,22 @@ def index2():
 
 @app.route("/#Testimoni")
 def testi():
-    with open(".//txt//1.txt",mode='r') as f1:
-        lins = f1.readline()
-    names1 = lins[0].rstrip('\n')
+    mytxt = parsetxt.Txtparse()
+    mytxt.opentxt("./static/txt/1.txt")
+    mytxt.readtxt()
+    author = mytxt.author
+    title = mytxt.title
+    contxt = mytxt.contxt
+    #get_flashed_messages(author)
+    #return render_template("TstWeb.html", author = author)
+    return author , title , contxt
+
+
+@app.route("/Dnlod")
+def donload():
+
+    return render_template("./Dnlod/ProdDnlod.html")
+
 
 
 
